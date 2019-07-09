6153d02c8e1ff0f1ca94a58d9d564ce6f765689c
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-09 22:48:21 +1000

    Update Player.h
    Final solution for Player Class

--- Player.h
+++ Player.h
@@ -1,17 +1,26 @@
 #pragma once
 #include <iostream>
+#include "Slot_Cosmetics.h"
 
 using namespace std;
 
 class Player
 {
 public:
-	int chips; //Integer to store player's chips
 	Player();
 	~Player();
+	//This function automatically sets the default player chips to 2000 when nothing is assigned to it
 	void setPlayerChips(int c = 2000);
 	int getPlayerChips();
-	bool doesPlayerHaveChips(); //Checks whether or not the player can continue playing
+	//Checks whether or not the player can continue playing
+	bool doesPlayerHaveChips(); 
+	//Displays players chip count
+	void dispChips(int playerChips); 
+private:
+	//Creating an instance of the Slot_Cosmetics class in order to use some of its functions
+	Slot_Cosmetics slotCos;
+	//Integer to store player's chips
+	int chips; 
 };
 
 Player::Player()
@@ -35,4 +44,11 @@ int Player::getPlayerChips()
 bool Player::doesPlayerHaveChips()
 {
 	return (chips > 0);
+}
+
+void Player::dispChips(int playerChips)
+{
+	slotCos.setColour(GREEN);
+	slotCos.tabToMiddle();
+	cout << "Player's chips: $" << playerChips << "\n";
 }
