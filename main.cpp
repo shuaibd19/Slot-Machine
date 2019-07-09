d4d2467bf0e343dd19e7e4aaf706a170995613f7
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-09 22:47:38 +1000

    update main.cpp
    final solution for main class

--- main.cpp
+++ main.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
-#include <string>
-#include <time.h>
 #include "Slot_Cosmetics.h"
 #include "Player.h"
-
+#include "Slot_Machine.h"
 using namespace std;
-enum MenuChoices
-{
-	PLAY_SLOTS = 1,
-	DISP_CREDITS,
-	QUIT
-};
 
 int main()
 {
-	Slot_Cosmetics slot_cosmetics;
+	bool playerHasQuit = false;
+	//Creating objects of the various classes needed to run the program
 	Player player;
+	Slot_Machine slot_machine;
+	Slot_Cosmetics slot_cosmetics;
+	//The setPlayerChips() function has a default of 2000 hence no assigning of any initial value
 	player.setPlayerChips();
-	cout << player.getPlayerChips() << endl;
+	//This condition of the loop ensures that the game exits either when the player decides to quit
+	//or their chip count is completely depleted to 0
+	while (!playerHasQuit && player.doesPlayerHaveChips())
+	{
+		player.dispChips(player.getPlayerChips());
+		slot_cosmetics.dispMenu();
+		int userInput = slot_machine.getUserInp();
+		switch (userInput)
+		{
+		case 1:
+			slot_cosmetics.dispWarning();
+			slot_cosmetics.dispDollarBills();
+			player.setPlayerChips(slot_machine.slotGame(player.getPlayerChips()));
+			break;
+		case 2:
+			slot_cosmetics.dispCredits();
+			break;
+		case 3:
+			playerHasQuit = true;
+			break;
+		default:
+			slot_cosmetics.dispInvalidChoice();
+		}
+	}
 	return 0;
 }
+
